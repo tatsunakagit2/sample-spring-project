d2063fe92e9b814805c6554361a4ef5b640edf54
tatsu <nakanaka_nakagawa7@yahoo.co.jp>
2022-04-10 18:24:38 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/SampleSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringProjectApplication.class, args);
+	}
+
+}
